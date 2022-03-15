e69ae79fa01b6da1a36b26a6b028bee20d56f6ca
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-03-15 14:23:50 -0500

    Adding "HTML" part
    Also removed some useless code that was there by default

---
+++ src/components/Calculator.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+export default function Calculator() {
+  return (
+    <div className="calculator-container">
+      <div>
+        <p>0</p>
+      </div>
+      <div>
+        <a href>AC</a>
+      </div>
+      <div>
+        <a href>+/-</a>
+      </div>
+      <div>
+        <a href>%</a>
+      </div>
+      <div>
+        <a href>÷</a>
+      </div>
+      <div>
+        <a href>7</a>
+      </div>
+      <div>
+        <a href>8</a>
+      </div>
+      <div>
+        <a href>9</a>
+      </div>
+      <div>
+        <a href>X</a>
+      </div>
+      <div>
+        <a href>4</a>
+      </div>
+      <div>
+        <a href>5</a>
+      </div>
+      <div>
+        <a href>6</a>
+      </div>
+      <div>
+        <a href>-</a>
+      </div>
+      <div>
+        <a href>1</a>
+      </div>
+      <div>
+        <a href>2</a>
+      </div>
+      <div>
+        <a href>3</a>
+      </div>
+      <div>
+        <a href>+</a>
+      </div>
+      <div>
+        <a href>0</a>
+      </div>
+      <div>
+        <a href>.</a>
+      </div>
+      <div>
+        <a href>=</a>
+      </div>
+    </div>
+  );
+}
