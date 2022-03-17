f7fad1c201075e059ccf322e5e0e060685de2dcc
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-03-17 13:29:52 -0500

    Refactoring to use hooks
    - There's no need to import react when importing useState.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,47 +1,40 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './Calculator.module.css';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.clickHandler = this.clickHandler.bind(this);
-  }
+export default function Calculator() {
+  const [total, setTotal] = useState(0);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
 
-  clickHandler(e) {
-    this.setState((state) => calculate(state, e.target.textContent));
+  function clickHandler(e) {
+    const result = calculate({ total, next, operation }, e.target.textContent);
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
   }
-
-  render() {
-    const { total, operation, next } = this.state;
-    const buttonList = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-    return (
-      <div className={styles.calculator}>
-        <div className={styles.calculator__result}>
-          <p>
-            {total}
-            {' '}
-            {operation}
-            {' '}
-            {next}
-          </p>
-        </div>
-        {buttonList.map((item, index) => {
-          let name = styles.calculator__button__grey;
-          if ((index + 1) % 4 === 0 || index === 18) { name = styles.calculator__button__orange; }
-          if (index === 16) { name = `${styles.calculator__button__grey} ${styles.calculator__button__zero}`; }
-          return (
-            <div key={item.id} className={name} onClick={this.clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') this.clickHandler(); }} role="button" tabIndex={0}>
-              <p style={{ userSelect: 'none' }}>{item}</p>
-            </div>
-          );
-        })}
+  const buttonList = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  return (
+    <div className={styles.calculator}>
+      <div className={styles.calculator__result}>
+        <p>
+          {total}
+          {' '}
+          {operation}
+          {' '}
+          {next}
+        </p>
       </div>
-    );
-  }
+      {buttonList.map((item, index) => {
+        let name = styles.calculator__button__grey;
+        if ((index + 1) % 4 === 0 || index === 18) { name = styles.calculator__button__orange; }
+        if (index === 16) { name = `${styles.calculator__button__grey} ${styles.calculator__button__zero}`; }
+        return (
+          <div key={item.id} className={name} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
+            <p style={{ userSelect: 'none' }}>{item}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
