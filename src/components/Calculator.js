30db131aa2e2f80c911828bfcee1bc14145b2240
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-03-17 12:47:55 -0500

    Refactored function component to class
    - Adding an array of symbols to loop from, that way the code is DRY!

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,87 +1,47 @@
-import { React, useState } from 'react';
+import React from 'react';
 import styles from './Calculator.module.css';
 import calculate from '../logic/calculate';
 
-export default function Calculator() {
-  const [total, setTotal] = useState(0);
-  const [next, setNext] = useState(null);
-  const [operation, setOperation] = useState(null);
-
-  function clickHandler(e) {
-    const result = calculate({ total, next, operation }, e.target.textContent);
-    setTotal(result.total);
-    setNext(result.next);
-    setOperation(result.operation);
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.clickHandler = this.clickHandler.bind(this);
   }
 
-  return (
-    <div className={styles.calculator}>
-      <div className={styles.calculator__result}>
-        <p>
-          { total }
-          { operation }
-          { next }
-        </p>
+  clickHandler(e) {
+    this.setState((state) => calculate(state, e.target.textContent));
+  }
+
+  render() {
+    const { total, operation, next } = this.state;
+    const buttonList = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+    return (
+      <div className={styles.calculator}>
+        <div className={styles.calculator__result}>
+          <p>
+            {total}
+            {' '}
+            {operation}
+            {' '}
+            {next}
+          </p>
+        </div>
+        {buttonList.map((item, index) => {
+          let name = styles.calculator__button__grey;
+          if ((index + 1) % 4 === 0 || index === 18) { name = styles.calculator__button__orange; }
+          if (index === 16) { name = `${styles.calculator__button__grey} ${styles.calculator__button__zero}`; }
+          return (
+            <div key={item.id} className={name} onClick={this.clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') this.clickHandler(); }} role="button" tabIndex={0}>
+              <p style={{ userSelect: 'none' }}>{item}</p>
+            </div>
+          );
+        })}
       </div>
-      {/* All buttons needed to have a keypress, role, and tabIndex to pass ESlint */}
-      {/* None <p> element is focusable, excepting the result one */}
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>AC</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>+/-</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>%</p>
-      </div>
-      <div className={styles.calculator__button__orange} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>÷</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>7</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>8</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>9</p>
-      </div>
-      <div className={styles.calculator__button__orange} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>x</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>4</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>5</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>6</p>
-      </div>
-      <div className={styles.calculator__button__orange} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>-</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>1</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>2</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>3</p>
-      </div>
-      <div className={styles.calculator__button__orange} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>+</p>
-      </div>
-      <div className={`${styles.calculator__button__grey} ${styles.calculator__button__zero}`} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>0</p>
-      </div>
-      <div className={styles.calculator__button__grey} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>.</p>
-      </div>
-      <div className={styles.calculator__button__orange} onClick={clickHandler} onKeyPress={(e) => { if (e.key === 'Enter') clickHandler(); }} role="button" tabIndex={0}>
-        <p style={{ userSelect: 'none' }}>=</p>
-      </div>
-    </div>
-  );
+    );
+  }
 }
