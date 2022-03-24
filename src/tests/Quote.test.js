035db18627bb5825d7515c162eea481ca6d96415
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-03-24 17:19:39 -0500

    Adding unit tests for all React components

---
+++ src/tests/Quote.test.js
@@ -0,0 +1,8 @@
+import renderer from 'react-test-renderer';
+import Quote from '../components/Quote';
+
+it('renders correctly', () => {
+  const tree = renderer.create(<Quote />).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
